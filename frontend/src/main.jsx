db115b6d2f6bf35203ca2b644441fa1e88daddd3
Sachinyadavsk <sachinsk4832499@gmail.com>
2025-10-08 12:44:43 +0530

    vifeo chat

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/strem-video-app">
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>

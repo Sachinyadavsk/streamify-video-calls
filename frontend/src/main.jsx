637f47ff20898e7450055056b8b36d2a5c6975fa
Sachinyadavsk <sachinsk4832499@gmail.com>
2025-10-08 13:02:50 +0530

    update code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,7 +12,7 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/strem-video-app">
+    <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
